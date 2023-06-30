108e90fc5db0eb2360c5a984ac3c3fc79efbf354
AntonioEduardo-Dev~ <antedu467@gmail.com>
2023-06-30 13:16:12 -0300

    feat: model user and model consumidor

---
+++ app/Models/Consumidor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Consumidor extends Authenticatable implements JWTSubject
+{
+    use HasFactory;
+    
+    protected $table = 'consumidor';
+
+    // Implementação dos métodos da interface JWTSubject
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
